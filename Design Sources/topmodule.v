490af272699081639e275887f3ef9a00339f3144
Navneet Prasad <navneetprasad1311@gmail.com>
2025-08-05 14:56:26 +0530

    topmodule.v

---
+++ Design Sources/topmodule.v
@@ -0,0 +1,18 @@
+`timescale 1ns / 1ps
+
+module top_module(input c_in,  
+                        reset,
+                        start,
+                        pause,
+                        lid,
+                  output [2:0] stage,
+                  output done,
+                  output input_valve,
+                  output output_drain);
+    
+    wire clk;
+  
+    clockdivider clk_div(.c_out(clk),.clk(c_in),.rst(reset));
+    AWMC awmc(.clk(clk),.reset(reset),.start(start),.pause(pause),.lid(lid),.stage(stage),.done(done),.input_valve(input_valve),.output_drain(output_drain));
+    
+endmodule
